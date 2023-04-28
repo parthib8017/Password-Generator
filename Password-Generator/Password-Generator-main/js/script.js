196bf87c609ebdb42461a28ef74d64fc5741a08c
parthib8017 <132046859+parthib8017@users.noreply.github.com>
2023-04-28 13:00:51 +0530

    Add files via upload

---
+++ Password-Generator/Password-Generator-main/js/script.js
@@ -0,0 +1,100 @@
+const btn = document.querySelector('.btn');
+const length = document.querySelector('#length');
+const lengthValue = document.querySelector('#length-value');
+const storangValue = document.querySelector('.storang-value');
+const digits = document.querySelector('#digits');
+const digitsValue = document.querySelector('#digits-value');
+const capitals = document.querySelector('#capitals');
+const capitalsValue = document.querySelector('#capitals-value');
+const symbols = document.querySelector('#symbols');
+const symbolsValue = document.querySelector('#symbols-value');
+const copy = document.querySelector('.copy');
+const textarea = document.querySelector('#textarea');
+const warning = document.querySelector('.warning');
+const generate = document.querySelector('.generate');
+
+btn.addEventListener('click' , () => {
+    window.open('https://github.com/parthib8017' , '_blank');
+});
+
+length.addEventListener('input' , () => {
+    if (length.value < 10) {
+        lengthValue.innerHTML = '0'+length.value;
+        storangValue.style.width = '10%';
+        storangValue.style.backgroundColor = 'red';
+
+    } if(length.value >= 10) {
+        lengthValue.innerHTML = length.value;
+        storangValue.style.width = '30%';
+        storangValue.style.backgroundColor = '#f53d3d';
+
+    } if (length.value >= 14){
+        lengthValue.innerHTML = length.value;
+        storangValue.style.width = '50%';
+        storangValue.style.backgroundColor = 'orange';
+
+    } if (length.value >= 18) {
+        lengthValue.innerHTML = length.value;
+        storangValue.style.width = '70%';
+        storangValue.style.backgroundColor = 'green';
+
+    } if (length.value >= 22) {
+        lengthValue.innerHTML = length.value;
+        storangValue.style.width = '90%';
+        storangValue.style.backgroundColor = '#38ef7d';
+
+    } if (length.value == 25) {
+        lengthValue.innerHTML = length.value;
+        storangValue.style.width = '100%';
+        storangValue.style.backgroundColor = '#38ef7d';
+    }
+});
+
+digits.addEventListener('input' , () => {
+    if(digits.value < 10){
+        digitsValue.innerHTML = '0'+digits.value;
+    }else {
+        digitsValue.innerHTML = digits.value;
+    }
+});
+
+capitals.addEventListener('input' , () => {
+    if(capitals.value < 10){
+        capitalsValue.innerHTML = '0'+capitals.value;
+    } else {
+        capitalsValue.innerHTML = capitals.value;
+    }
+});
+
+symbols.addEventListener('input' , () => {
+    symbolsValue.innerHTML = '0'+symbols.value;
+    console.log(symbolsValue);
+});
+
+copy.addEventListener('click' , () => {
+    textarea.select();
+    let textareaValue = textarea.value;
+    navigator.clipboard.writeText(textareaValue);
+    copy.innerHTML = 'Copied!';
+    copy.classList.remove('copy');
+    copy.classList.add('copied');
+    setTimeout(() => {
+        copy.innerHTML = 'Copy';
+        copy.classList.remove('copied');
+        copy.classList.add('copy');
+    } , 2500);
+});
+
+function passGenerate(){
+    let result = '';
+    let char = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789!@#$%_-*';
+    for(let i = 0 ; i < length.value ; i++){
+        result += char.charAt(Math.floor(Math.random() * char.length));
+    }
+    return result;
+}
+
+generate.addEventListener('click' , () => {
+    textarea.innerHTML = passGenerate();
+    console.log(length.value)
+})
